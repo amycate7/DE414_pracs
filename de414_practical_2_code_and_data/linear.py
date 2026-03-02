9c841624439efaf9f82b2e7653bc0920cc4a4505
Amy Cate Mc Dermott <26911264@sun.ac.za>
2026-03-02 16:18:58 +0200

    Add code

--- de414_practical_2_code_and_data/linear.py
+++ de414_practical_2_code_and_data/linear.py
@@ -42,17 +42,17 @@ def read_mnist_csv(filename):
             X.append(feats)
 
             # add the targets to array
-            one_hot = np.zeros(10)
-            one_hot[int(clean_line[0])] = 1
+            one_hot = np.zeros(10) # create a one-hot encoded vector of size 10 (number of classes)
+            one_hot[int(clean_line[0])] = 1 # set the index of the target class (read from target variable) to 1
             y.append(one_hot)
 
     # convert to numpy arrays and normalise training features
     X = np.array(X) / 2550
-    y = np.array(y)
+    y = np.array(y) # array of shape (N,10) where N is the number of samples and 10 is the number of classes (one-hot encoding)
 
     return X, y
 
-# import the data from a numpy archive - MUST FASTER!!!
+# import the data from a numpy archive - MUCH FASTER!!!
 def read_mnist_npz(filename):
     """
     Reads the MNISTS dataset from a numpy archive.
@@ -82,12 +82,19 @@ def read_mnist_npz(filename):
 
     data = np.load(filename)
 
-    X = data['X']
-    Y = data['Y']
+    X = data['X'] # Extract the feature vectors
+    Y = data['Y'] # Extract the target vectors
 
     return X,Y
 
 #************************** YOUR CODE HERE ************************#
+
+X_data = read_mnist_npz("./data/train.npz")[0] # read the data from the numpy archive
+Y_data = read_mnist_npz("./data/train.npz")[1] # read the target data from the numpy archive
+print(f"Dimensions of our feature matrix: {X_data.shape}")
+print(f"Dimensions of our target matrix: {Y_data.shape}")
+
+# Note: N = 60000, D = 784, K = 10
 
 #******************************************************************#
 
@@ -144,6 +151,7 @@ class LinearRegression:
             self.weights : numpy.ndarray of shape (D,K) where D
                 is dimensionality of of weight vector (including
                 bias) and K is the number of classes
+                The model predicts K numbers (one per class). So for N samples, we want an (N,K) output.
         """
 
         self.D = D + 1 # add 1 to size for bias
@@ -157,6 +165,7 @@ class LinearRegression:
     def forward(self, X):
         """
         Computes the forward pass through the model
+        Use input data (X) and model weights to compute the output of the model (y_hat)
 
         Parameters:
         -----------
@@ -178,6 +187,8 @@ class LinearRegression:
             "but recieved shape: " + str(X.shape))
                 
         #************************** YOUR CODE HERE ************************#
+        y_out = X @ self.weights
+        return y_out
 
         #******************************************************************#
 
@@ -209,7 +220,8 @@ class LinearRegression:
         #     (2.a) Implement the least-squares regression solution        #
         ####################################################################
 
-        #************************** YOUR CODE HERE ************************#
+        weights_opt = np.linalg.inv(X.T @ X) @ X.T @ Y # compute the optimal weights using the normal equations
+        self.weights = weights_opt # set the model weights to the optimal weights
 
         #******************************************************************#
 
@@ -247,7 +259,8 @@ class LinearRegression:
 #              (2.c) Create and train the model                    #
 ####################################################################
 
-#************************** YOUR CODE HERE ************************#
+lr = LinearRegression(X_data.shape[1], Y_data.shape[1]) # create the model with D = 784 and K = 10
+lr.train_normaleqs(X_data,Y_data) # train the model using the normal equations
 
 #******************************************************************#
 
@@ -269,14 +282,27 @@ def accuracy(Y,Y_hat):
             - Digit probabilities/likelihoods for each example in Y
     """
 
-    Y_hat = np.argmax(Y_hat,-1) # get ids of predictions
-    Y = np.argmax(Y,-1) # get ids of targets
+    Y_hat = np.argmax(Y_hat,-1) # get ids of predictions: for each row, retrieve index of the column that has the highest score
+    Y = np.argmax(Y,-1) # get ids of targets: ground truth labels
 
     acc = np.sum(Y_hat == Y) # count the number of times the ids are the same
 
     return acc / Y.shape[0] # normalise by the total # targets
 
-#************************** YOUR CODE HERE ************************#
+y_hat = lr.forward(X_data)
+print(f"Trained model accuracy: {accuracy(Y_data, y_hat)}")
+# This results in an accuracy of 0.85525
+
+# Randomly choosing a prediction for a baseline comparison -- every trained classidier should perform better than this
+num_samples = Y_data.shape[0]
+np.random.seed(42) # set random seed for reproducibility
+random_predictions = np.random.randint(0, 10, size=num_samples) # generate random predictions for each sample
+# Convert to NxK matrix
+random_predictions_matrix = np.zeros((num_samples, 10))
+for i in range(num_samples):
+    random_predictions_matrix[i, random_predictions[i]] = random_predictions[i]
+print(f"Untrained model accuracy: {accuracy(Y_data, random_predictions_matrix)}")
+# The result in accuracy is 0.10063333333333334 -- consistent with intuition -- random guessing for a 10 class classification problem should result in success 1/10 times
 
 #******************************************************************#
 
@@ -284,7 +310,21 @@ def accuracy(Y,Y_hat):
 #                (4) Calculate the model error                     #
 ####################################################################
 
-#************************** YOUR CODE HERE ************************#
+def error(y, y_hat):
+    """
+    Calculate the squared error.
+
+    Arguments:
+    ----------
+        y: np.array of shape (N,K)
+            - One-hot encoded targets
+        y_hat: np.array of shape (N,K)
+            - Digit probabilities/likelihoods for each example in y
+    """
+    return np.sum((y - y_hat)**2)
+
+rand_pred_error = error(Y_data, random_predictions_matrix)
+print(f"Random model error: {rand_pred_error}")
 
 #******************************************************************#
 
